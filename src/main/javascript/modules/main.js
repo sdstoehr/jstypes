7392aeea5b2b2ec8d2120c440437ed18cd8776a7
Sebastian Stöhr <sdstoehr@gmx.de>
2014-10-18 23:13:58 +0200

    open external links in new window, track outgoing links

--- src/main/javascript/modules/main.js
+++ src/main/javascript/modules/main.js
@@ -1,5 +1,37 @@
 var main = (function (_window, undefined) {
 
+    var _document = _window.document;
+
+    (function externalLinks() {
+
+        var links = _document.querySelectorAll('a[rel*="external"]'), i, link, track;
+
+        track = function (e) {
+            var target = e.target, href, _ga = _window.ga;
+            if (!target) {
+                return;
+            }
+
+            href = target.href;
+
+            if (!href) {
+                return;
+            }
+
+            _ga('send', 'event', 'link', 'click', href);
+
+        };
+
+        for (i = 0; i < links.length; i = i + 1) {
+            link = links[i];
+            link.setAttribute('target', '_blank');
+            link.onclick = track;
+        }
+
+    }());
+
+
+
   return {};
 
 }(window));
